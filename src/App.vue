be24fc42b8739d31d872ba7897a69f1ca75b3796
blur64 <shalomsi922@gmail.com>
2024-04-01 02:38:41 +0400

    add signals sequence execution logic

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,29 @@
 <template></template>
 
 <script>
+import SimonMachine from "./SimonMachine";
+
 export default {
   name: "App",
+  data() {
+    return {
+      simonMachine: new SimonMachine({
+        onSingleSignalOutput: this.handleSignal,
+        onSequenceExecutionFinish: this.handleFinish,
+      }),
+    };
+  },
+  methods: {
+    handleSignal(signal) {
+      console.log(signal);
+    },
+    handleFinish() {
+      console.log("machine is finished");
+    },
+  },
+  created() {
+    this.simonMachine.start();
+  },
 };
 </script>
 
--- src/SimonMachine.js
+++ src/SimonMachine.js
@@ -7,8 +7,58 @@ const simonMachineStates = {
 
 // const simonMachineSignals = {}; ??
 
-class SimonMachine {
-  constructor(onSingleSignalOutput, onSequenceExecutionFinish, onWrongInputSignal) { }
+export default class SimonMachine {
+  _onSingleSignalOutput = null;
+  _onSequenceExecutionFinish = null;
+  _onWrongInputSignal = null;
+
+  _state = simonMachineStates.READY_TO_START;
+  _currentSignalsSequence = [];
+  _signalOutputTimeInterval = 1500;
+  _HTMLIntervalId = 0;
+
+  constructor({ onSingleSignalOutput, onSequenceExecutionFinish, onWrongInputSignal }) {
+    this._onSingleSignalOutput = onSingleSignalOutput;
+    this._onSequenceExecutionFinish = onSequenceExecutionFinish;
+    this._onWrongInputSignal = onWrongInputSignal;
+  }
+
+  _getRandomSignal() {
+    return generateRandomIntFromZeroTo(3);
+  }
+
+  _outputSignal(numberOfSignalToOutput) {
+    const signalToOutput = this._currentSignalsSequence[numberOfSignalToOutput];
+    this._onSingleSignalOutput(signalToOutput);
+  }
+
+  _finishExecutionIfSignalIsLast(signalNumber) {
+    if (signalNumber === this._currentSignalsSequence.length) {
+      this._onSequenceExecutionFinish();
+      clearInterval(this._HTMLIntervalId);
+      this._state === simonMachineStates.INPUT_WAITING;
+    }
+  }
+
+  _executeSequence() {
+    this._currentSignalsSequence.push(this._getRandomSignal());
+    let signalNumber = 0;
+    this._HTMLIntervalId = setInterval(() => {
+      this._outputSignal(signalNumber++);
+      this._finishExecutionIfSignalIsLast(signalNumber);
+    }, this._signalOutputTimeInterval);
+  }
+
   input(signal) { }
-  start() { }
+
+  start() {
+    if (this._state === simonMachineStates.READY_TO_START) {
+      this._state = simonMachineStates.SEQUNCE_EXECUTION;
+      this._executeSequence();
+    }
+  }
+}
+
+function generateRandomIntFromZeroTo(to) {
+  return Math.round(Math.random() * to);
 }
