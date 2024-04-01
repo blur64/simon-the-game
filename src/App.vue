8a173a05d9b29b9880ea6e893e3e74bba2037ada
blur64 <shalomsi922@gmail.com>
2024-04-01 15:59:42 +0400

    add buttons styles

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
-    <button
-      v-for="(signal, idx) of simonMachineSignals"
-      :key="idx"
-      @click="handleSignalBtnClicked(signal)"
-      :class="{ active: activeSignal === signal }"
-    >
-      {{ signal }}
-    </button>
+    <div class="buttons-wrapper">
+      <button
+        v-for="(signal, idx) of simonMachineSignals"
+        :key="idx"
+        @click="handleSignalBtnClicked(signal)"
+        :class="{ active: activeSignal === signal }"
+        class="button"
+      ></button>
+    </div>
     <button @click="handleStartBtnClicked">start</button>
     <span>{{ round }}</span>
   </div>
@@ -58,5 +59,59 @@ export default {
 <style>
 .active {
   background-color: palevioletred;
+}
+
+.buttons-wrapper {
+  width: 200px;
+  position: relative;
+}
+
+.button {
+  width: 100px;
+  height: 100px;
+  opacity: 0.55;
+  border: 1px solid grey;
+  padding: 0;
+}
+
+.button:hover {
+  cursor: pointer;
+}
+
+.button:first-child {
+  border-top-left-radius: 9999px;
+  background-color: blue;
+}
+.button:nth-child(2) {
+  border-top-right-radius: 9999px;
+  background-color: red;
+}
+.button:nth-child(3) {
+  border-bottom-left-radius: 9999px;
+  background-color: green;
+}
+.button:last-child {
+  border-bottom-right-radius: 9999px;
+  background-color: yellow;
+}
+
+.button:active {
+  opacity: 1;
+  position: relative;
+}
+
+.button:first-child:active,
+.button:nth-child(2):active {
+  top: 2px;
+}
+
+.button:last-child:active,
+.button:nth-child(3):active {
+  bottom: 2px;
+}
+
+.active {
+  opacity: 1;
+  border: 1px solid #333;
 }
 </style>
